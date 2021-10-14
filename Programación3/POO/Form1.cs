cebcac6f4c164239b0fe604370c66b8ff1f0f87e
CesarCruz73 <cruzcesarbanegas@outlook.com>
2021-10-14 00:08:53 -0600

    Primera clase de POO
    Primera clase viendo el tema Programación Orientada a Objetos en Lenguaje III con C#

---
+++ Programación3/POO/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Usuario usuario = new Usuario("CRUZ", "cesarcruz@poo.com");
+
+            Usuario usuario = new Usuario();
+            usuario.Codigo = "CRUZ";
+            usuario.Correo = "cesarcruz@poo.com";
+            usuario.Nombre = "CÉSAR";
+            usuario.Apellido = "CRUZ";
+
+            MessageBox.Show(usuario.GetNombreCompleto(usuario.Nombre, usuario.Apellido));
+
+        }
+    }
+}
---
+++ Programación3/POO/Usuario.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Usuario
+    {
+        //Propiedades, Características, Atributos
+
+        //Atributos.
+        private string apellidos;
+
+
+
+
+        //Propiedades
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido
+        {
+            set
+            {
+                apellidos = value;
+            }
+            get
+            {
+                return apellidos;
+            }
+
+        }
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+
+        //Constructores
+        public Usuario()
+        {
+        }
+
+        public Usuario(string codigo, string correo)
+        {
+            Codigo = codigo;
+            Correo = correo;
+
+        }
+
+        public Usuario(string codigo, string nombre, string apellido, int edad, string correo, string contraseña) : this(codigo, nombre)
+        {
+            Apellido = apellido;
+            Edad = edad;
+            Correo = correo;
+            Contraseña = contraseña;
+        }
+
+        //Métodos
+
+        public string GetNombreCompleto(string nombre, string apellidos)
+        {
+            string nombreCompleto = nombre + " " + apellidos;
+            return nombreCompleto;
+        }
+
+    }
+}
